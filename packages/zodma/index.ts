5c04b34e6cffcab66cd96269ce164a389804ed02
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2023-01-29 19:40:13 -0800

    stuff

--- packages/zodma/index.ts
+++ packages/zodma/index.ts
@@ -1,4 +1,4 @@
-import z, { any, type ZodRawShape } from "zod";
+import z, { type ZodRawShape } from "zod";
 
 declare module "zod" {
   interface ZodString {
@@ -23,7 +23,7 @@ type ConstraintFullOptions<T> = {
 } & ConstraintAdditionalOptions;
 
 class ConstraintBuilder<T> {
-  constructor() {}
+  constructor() { }
 
   index(
     fields: ConstraintFields<T>,
@@ -53,7 +53,7 @@ class ConstraintBuilder<T> {
 }
 
 class TableBuilder {
-  constructor() {}
+  constructor() { }
 
   columns<T extends ZodRawShape>(columns: T) {
     const zodObject = z.object(columns);
@@ -62,47 +62,59 @@ class TableBuilder {
   }
 }
 
-export const table = new TableBuilder();
+class ZodmaBuilder {
+  constructor() { }
 
-zodma({
-  account: table
-    .columns({
-      id: z.string().uuid().primaryKey(),
-      userId: z.string().unique(),
-      type: z.string(),
-      provider: z.string(),
-      providerAccountId: z.string(),
-      refresh_token: z.string().optional(),
-      access_token: z.string().optional(),
-      expires_at: z.number().optional(),
-      token_type: z.string().optional(),
-      scope: z.string().optional(),
-      id_token: z.string().optional(),
-      session_state: z.string().optional(),
-    })
-    .unique(["provider", "providerAccountId"]),
-  session: table.columns({
-    id: z.string().uuid().primaryKey(),
-    sessionToken: z.string().unique(),
-    userId: z.string(),
-    expires: z.string(),
-  }),
-  user: table.columns({
-    id: z.string().uuid().primaryKey(),
-    name: z.string(),
-    email: z.string().optional().unique(),
-    emailVerified: z.date().optional(),
-    image: z.string().optional(),
-  }),
-  verificationToken: table
-    .columns({
-      identifier: z.string(),
-      token: z.string().unique(),
-      expires: z.date(),
-    })
-    .unique(["identifier", "token"]),
-});
-
-function zodma<T>(definition: T) {
-  return null;
+  config(opts: { databaseUrl: string }) {
+    return {
+      schema<T>(definition: T) { },
+    };
+  }
 }
+
+export const table = new TableBuilder();
+export const zodma = new ZodmaBuilder();
+
+zodma
+  .config({
+    databaseUrl: "postgres://localhost:5432",
+  })
+  .schema({
+    account: table
+      .columns({
+        id: z.string().uuid().primaryKey(),
+        userId: z.string().unique(),
+        type: z.string(),
+        provider: z.string(),
+        providerAccountId: z.string(),
+        refresh_token: z.string().optional(),
+        access_token: z.string().optional(),
+        expires_at: z.number().optional(),
+        token_type: z.string().optional(),
+        scope: z.string().optional(),
+        id_token: z.string().optional(),
+        session_state: z.string().optional(),
+      })
+      .unique(["provider", "providerAccountId"]),
+    session: table.columns({
+      id: z.string().uuid().primaryKey(),
+      sessionToken: z.string().unique(),
+      userId: z.string(),
+      expires: z.string(),
+    }),
+    user: table.columns({
+      id: z.string().uuid().primaryKey(),
+      name: z.string(),
+      email: z.string().optional().unique(),
+      emailVerified: z.date().optional(),
+      image: z.string().optional(),
+    }),
+    verificationToken: table
+      .columns({
+        identifier: z.string(),
+        token: z.string().unique(),
+        expires: z.date(),
+      })
+      .unique(["identifier", "token"]),
+  });
+
